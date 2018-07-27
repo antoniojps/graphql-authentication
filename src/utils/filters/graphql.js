08bc93db1c204216a319d4a03a04ee0365b24d80
António Santos <antoniojps@ua.pt>
2018-07-27 15:44:23 +0100

    fix secure filter

--- src/utils/filters/graphql.js
+++ src/utils/filters/graphql.js
@@ -3,10 +3,11 @@ import { AuthenticationError, ForbiddenError } from 'apollo-server-express'
 export function secure (func, admin = false, moderator = false) {
   return (root, args, context) => {
     if (!context.user) throw new AuthenticationError('Unauthenticated')
-    if (
-      (admin && !context.user.admin) ||
-      (moderator && !context.user.moderator)
-    ) {
+    // admin only
+    if ((admin && !moderator) && !context.user.admin) {
+      throw new ForbiddenError('Unauthorized')
+    // admin or moderator only
+    } else if ((admin && moderator) && (!context.user.admin && !context.user.moderator)) {
       throw new ForbiddenError('Unauthorized')
     }
     return func(root, args, context)
